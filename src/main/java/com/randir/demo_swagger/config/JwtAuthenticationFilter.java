a0b3074198c590197b6a590efe0ad5a344c28761
Diego Ramos <diegoa.ramosl@gmail.com>
2025-04-11 22:52:55 -0400

    Add authentication middleware

---
+++ src/main/java/com/randir/demo_swagger/config/JwtAuthenticationFilter.java
@@ -0,0 +1,36 @@
+package com.randir.demo_swagger.config;
+
+import java.io.IOException;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import com.randir.demo_swagger.service.JwtService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final HandlerExceptionResolver handlerExceptionResolver;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+    }
+
+}
--- src/main/java/com/randir/demo_swagger/repository/UserRepository.java
+++ src/main/java/com/randir/demo_swagger/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.randir.demo_swagger.entity.User;
 
-public interface UserRepository extends CrudRepository {
+public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 
 }
